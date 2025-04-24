9ff1ef57c492242e535fe964093c37cf9c0d25a1
das-ag <das.ag@northeastern.edu>
2025-04-24 01:17:44 -0400

    bad yolo bench
    Signed-off-by: das-ag <das.ag@northeastern.edu>

--- our_imp_v3/utils.py
+++ our_imp_v3/utils.py
@@ -12,56 +12,6 @@ import torch.optim as optim
 
 # -------------- Data Untils -------------------
 
-def parse_annotation(annotation_path, image_dir, img_size):
-    '''
-    Traverse the xml tree, get the annotations, and resize them to the scaled image size
-    '''
-    img_h, img_w = img_size
-
-    with open(annotation_path, "r") as f:
-        tree = ET.parse(f)
-
-    root = tree.getroot()  
-    
-    img_paths = []
-    gt_boxes_all = []
-    gt_classes_all = []
-    # get image paths
-    for object_ in root.findall('image'):
-        img_path = os.path.join(image_dir, object_.get("name"))
-        img_paths.append(img_path)
-      
-        # get raw image size    
-        orig_w = int(object_.get("width"))
-        orig_h = int(object_.get("height"))
-            
-        # get bboxes and their labels   
-        groundtruth_boxes = []
-        groundtruth_classes = []
-        for box_ in object_.findall('box'):
-            xmin = float(box_.get("xtl"))
-            ymin = float(box_.get("ytl"))
-            xmax = float(box_.get("xbr"))
-            ymax = float(box_.get("ybr"))
-        
-            # rescale bboxes
-            bbox = torch.Tensor([xmin, ymin, xmax, ymax])
-            bbox[[0, 2]] = bbox[[0, 2]] * img_w/orig_w
-            bbox[[1, 3]] = bbox[[1, 3]] * img_h/orig_h
-        
-            groundtruth_boxes.append(bbox.tolist())
-
-            # get labels
-            label = box_.get("label")
-            groundtruth_classes.append(label)
-
-        gt_boxes_all.append(torch.Tensor(groundtruth_boxes))
-        gt_classes_all.append(groundtruth_classes)
-                
-    return gt_boxes_all, gt_classes_all, img_paths
-
-# -------------- Prepocessing utils ----------------
-
 def calc_gt_offsets(pos_anc_coords, gt_bbox_mapping):
     pos_anc_coords = ops.box_convert(pos_anc_coords, in_fmt='xyxy', out_fmt='cxcywh')
     gt_bbox_mapping = ops.box_convert(gt_bbox_mapping, in_fmt='xyxy', out_fmt='cxcywh')
